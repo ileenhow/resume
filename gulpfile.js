3c6c8c013f8ce3097f476e6b5b5a28ce79185875
ileenhow <ileenhow@gmail.com>
2015-12-14 15:21:45 +0800

    set up basic project

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function() {
+	gulp.src('./sass/**/*.scss')
+		.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+		.pipe(gulp.dest('./css'));
+});
+ 
+gulp.task('watchSass', function() {
+	gulp.watch('./sass/**/*.scss', ['sass']);
+});
+
+gulp.task('default', ['sass', 'watchSass']);
